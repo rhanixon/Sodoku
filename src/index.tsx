bea3763411b91f3044c753c173460ac473bdb448
Bobby Nixon <rhanixon1@gmail.com>
2020-04-30 14:26:19 -0400

    more structural and styling buildout

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { ThemeProvider } from "styled-components"
 
+import { Content, Title, Card } from "./components"
 import { unregister } from "./core"
+import { GlobalStyles, theme } from "./styles"
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById("root"))
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Content>
+            <Title>Sodoku</Title>
+            <Card>The board will go</Card>
+        </Content>
+    </ThemeProvider>,
+    document.getElementById("root")
+)
 
 unregister()
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+    colors: {
+        background: "radial-gradient(#282c34cc, #282c34)",
+        black: "#2892c34",
+        blue: "#a0e9fd",
+        lightBlue: "#caf3fe",
+        white: "white",
+    },
+    transitions: "0.3s",
+}
