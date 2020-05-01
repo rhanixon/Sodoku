653a9d251c2d4335484863b02795a3f25ffe4d81
Bobby Nixon <rhanixon1@gmail.com>
2020-05-01 13:25:03 -0400

    added grid mapping of values in redux

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react"
-
+import { useSelector } from "react-redux"
+import { IReducer } from "reducers"
+import { N } from "typings"
 import { Container } from "./styles"
 
 interface IProps {
@@ -7,8 +9,15 @@ interface IProps {
     rowIndex: number
 }
 
+interface IState {
+    value: N
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-    return <Container></Container>
+    const state = useSelector<IReducer, IState>(({ grid }) => ({
+        value: grid ? grid[rowIndex][colIndex] : 0,
+    }))
+    return <Container>{state.value}</Container>
 }
 
 export default Block
--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,14 +1,16 @@
-import React, { FC, Children } from "react"
-
-import { GRID } from "typings"
-import { createFullGrid, fillGrid } from "utils"
-
+import React, { FC, Children, useCallback, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { AnyAction, Dispatch } from "redux"
+import { createGrid } from "reducers"
 import Block from "./block"
 import { Container, Row } from "./styles"
 
 const Grid: FC = () => {
-    const grid = createFullGrid()
-    console.log(grid)
+    const dispatch = useDispatch<Dispatch<AnyAction>>()
+    const create = useCallback(() => dispatch(createGrid()), [dispatch])
+    useEffect(() => {
+        create()
+    }, [create])
 
     return (
         <Container>
